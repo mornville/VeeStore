27205c2a277f5c50e620711efcaf89ad1aaae5d2
Ashutosh Jha <aj97389@gmail.com>
2019-04-13 21:06:13 +0530

    new Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
  <!-- Header store name with background -->
-    <appHeader store_name="Sadda Adda" store_desc="Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life" style="padding:20px"></appHeader>
+    <appHeader  style="padding:20px"></appHeader>
     <appMenu></appMenu>
       </div>
 </template>
